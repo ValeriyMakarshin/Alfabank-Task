fae5899c9e8f503f14abc606c92037879b4411d9
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-02-23 15:04:17 +0300

    Initial commit

---
+++ app/src/main/java/com/hodzi/alfabank_task/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.hodzi.alfabank_task
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AppCompatActivity
+import android.view.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+
+
+}
