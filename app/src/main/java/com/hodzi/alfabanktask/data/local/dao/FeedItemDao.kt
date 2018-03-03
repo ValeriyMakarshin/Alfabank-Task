aa6049bd45f9eaf613f3914aab3b79c419d90007
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-03 10:58:38 +0300

    Fixed sort by bookmark

--- app/src/main/java/com/hodzi/alfabanktask/data/local/dao/FeedItemDao.kt
+++ app/src/main/java/com/hodzi/alfabanktask/data/local/dao/FeedItemDao.kt
@@ -7,18 +7,21 @@ import io.reactivex.Flowable
 
 @Dao
 abstract class FeedItemDao {
-    @Query("SELECT * FROM feed_items")
+    @Query("SELECT * FROM feed_items ORDER BY bookmark")
     abstract fun loadAllFeedItems(): Flowable<List<FeedItemEntity>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     abstract fun insertAll(feedItemEntities: List<FeedItemEntity>)
+
+    @Query("DELETE FROM feed_items WHERE bookmark = 0")
+    abstract fun deleteWithoutBookmark()
 
     @Query("DELETE FROM feed_items")
     abstract fun deleteAll()
 
     @Transaction
     open fun updateFeedItem(feedItemEntities: List<FeedItemEntity>) {
-        deleteAll()
+        deleteWithoutBookmark()
         insertAll(feedItemEntities)
     }
 
