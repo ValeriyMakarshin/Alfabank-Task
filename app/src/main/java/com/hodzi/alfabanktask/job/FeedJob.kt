6e55c42578e05f7101dabf2bc1ee6f8802eb04bf
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-03 12:28:07 +0300

    Removed todo

--- app/src/main/java/com/hodzi/alfabanktask/job/FeedJob.kt
+++ app/src/main/java/com/hodzi/alfabanktask/job/FeedJob.kt
@@ -10,7 +10,6 @@ class FeedJob @Inject constructor(val interactor: Interactor) : Job() {
 
     companion object {
         const val TAG = "feedJobTag"
-        //TODO: change period
         private const val PERIODIC_TIME_MS = 60_000L
 
         fun startScheduleJob() {
