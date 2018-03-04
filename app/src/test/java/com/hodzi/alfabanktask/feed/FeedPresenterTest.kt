13f360c08db40a0f1b01aa466cac756cdd48799f
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-04 12:29:10 +0300

    Added test for FeedPresenter

---
+++ app/src/test/java/com/hodzi/alfabanktask/feed/FeedPresenterTest.kt
@@ -0,0 +1,71 @@
+package com.hodzi.alfabanktask.feed
+
+import com.hodzi.alfabanktask.RxHook
+import com.hodzi.alfabanktask.data.local.FeedItemEntity
+import com.hodzi.alfabanktask.interactor.Interactor
+import com.nhaarman.mockito_kotlin.*
+import io.reactivex.Flowable
+import io.reactivex.disposables.Disposable
+import org.junit.*
+
+
+class FeedPresenterTest {
+    @Rule @JvmField
+    val schedulers = RxHook()
+
+    val mockInteractor: Interactor = mock()
+    val feedPresenter: FeedPresenter = FeedPresenter(mockInteractor)
+
+    @Before fun setUp() {
+
+    }
+
+    @After fun tearDown() {
+    }
+
+    @Test fun loadDataTest() {
+        val expectedFeedItems = ArrayList<FeedItemEntity>()
+        doReturn(Flowable.just(expectedFeedItems))
+            .`when`(mockInteractor).getDbList()
+        val mockFeedView: FeedContract.View = mock()
+        feedPresenter.view = mockFeedView
+
+        feedPresenter.loadData()
+
+        verify(mockFeedView).showList(expectedFeedItems)
+        Assert.assertEquals(expectedFeedItems, feedPresenter.list)
+    }
+
+    @Test fun refreshTest() {
+        val mockFeedView: FeedContract.View = mock()
+        feedPresenter.view = mockFeedView
+
+        feedPresenter.refresh()
+
+        verify(mockInteractor).refresh(any(), any())
+    }
+
+    @Test fun unsubscribeSubscriptionTest() {
+        val mockDisposable: Disposable = mock {
+            on { isDisposed }.doReturn(false)
+        }
+        feedPresenter.disposable = mockDisposable
+
+        feedPresenter.unsubscribeSubscription()
+
+        verify(mockDisposable).dispose()
+        Assert.assertNull(feedPresenter.disposable)
+    }
+
+    @Test fun unsubscribeSubscriptionTestDisposed() {
+        val mockDisposable: Disposable = mock {
+            on { isDisposed }.doReturn(true)
+        }
+        feedPresenter.disposable = mockDisposable
+
+        feedPresenter.unsubscribeSubscription()
+
+        verify(mockDisposable, never()).dispose()
+        Assert.assertEquals(feedPresenter.disposable, mockDisposable)
+    }
+}
